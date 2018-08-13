088544a82e94e95f41e8f7c9b9cb7061f8d0c7fc
Bruno Moreira De Barros <bruno@turnkeyit.co.uk>
2018-08-13 09:16:58 +0100

    Fixes an issue that was causing temp files to be deleted immediately upon bootstrapping.

--- bootstrap.php
+++ bootstrap.php
@@ -1,3 +1,5 @@
 <?php
 
-new Brunodebarros\FixPhpPostInput\FixPhpPostInput();
+# We use a global variable here to make sure the __destruct is only called at the end of the script's execution.
+# Otherwise, it'd be called right after the execution of this file, which would delete all temp files.
+$GLOBALS["d094efdc456faca4f987b6fc6e44c6c10a892765"] =  new Brunodebarros\FixPhpPostInput\FixPhpPostInput();
